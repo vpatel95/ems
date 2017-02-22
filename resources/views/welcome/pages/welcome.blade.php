1faaed139d39fb94dee0cd291d0d412d614b764c
Ved Patel <vedmpatel@gmail.com>
2017-02-22 23:29:31 +0530

    Welcome update

--- resources/views/welcome/pages/welcome.blade.php
+++ resources/views/welcome/pages/welcome.blade.php
@@ -4,16 +4,9 @@
 
 @section('content')
 <div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @if (Auth::check())
-                <a href="#">{{ $user->name }}</a>
-                <a href="{{ url('/logout') }}">Logout</a>
-            @else
-                <a href="{{ url('/home') }}">Login/Register</a>
-            @endif
-        </div>
-    @endif
+    <div class="top-right links">
+        <a href="{{ url('/home') }}">Login/Register</a>
+    </div>
 
     <div class="content">
         <div class="title m-b-md">
