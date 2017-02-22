75dd9aa85f7864bbc96b8eb3d2c16005468ee4ec
Ved Patel <vedmpatel@gmail.com>
2017-02-23 04:24:35 +0530

    Added Database Seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run() {
         
-        $this->call(UserTableSeeder::class);
-        $this->call(HodTableSeeder::class);
+    	factory(App\User::class, 10000)->create();
+
     }
 }
