8ee77ac5f8d41fbb06872108358cb973d0e97a3b
Ved Patel <vedmpatel@gmail.com>
2017-02-26 00:21:22 +0530

    Admin update

--- resources/views/loginReg/pages/admin.blade.php
+++ resources/views/loginReg/pages/admin.blade.php
@@ -32,5 +32,44 @@
 			    </div>
 			@endif
 		</div> 
+		<div class="login_panel reg_section" style="display:none">
+			<div class="login_head">
+				<h1>Add Admin</h1>
+			</div>
+			<form action="{{ route('admin.add') }}" method="POST">
+				<div class="form-group">
+					<label for="name">Name</label>
+					<input type="text" id="name" name="name" class="form-control input-lg">
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" id="email" name="email" class="form-control input-lg">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" id="password" name="password" class="form-control input-lg">
+				</div>
+				<div class="form-group">
+					<label for="re_password">Retype Password</label>
+					<input type="password" id="re_password" name="re_password" class="form-control input-lg">
+				</div>
+				<div class="login_submit">
+					<button class="btn btn-primary btn-block btn-lg">Add Admin</button>
+					<input type="hidden" name="_token" value="{{ Session::token() }}">
+				</div>
+				<div class="text-center">
+					<small>Never mind, <a class="form_toggle" href="#login_form">send me back to the sign-in screen</a></small>
+				</div>
+			</form>
+			@if (count($errors) > 0)
+			    <div class="alert alert-danger">
+			        <ul>
+			            @foreach ($errors->all() as $error)
+			                <li>{{ $error }}</li>
+			            @endforeach
+			        </ul>
+			    </div>
+			@endif
+		</div>
 	</div>
 @endsection
